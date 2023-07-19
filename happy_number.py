1abbd9a7a59a14707aa54377f23a7282fd00c43c
Camilo Chaves <camilo@Camilos-MacBook-Air.local>
2023-07-19 09:51:29 -0700

    happy_number.py

---
+++ happy_number.py
@@ -0,0 +1,25 @@
+'''Write an algorithm to determine if a number n is happy.
+
+A happy number is a number defined by the following process:
+
+Starting with any positive integer, replace the number by the sum of the 
+squares of its digits.
+Repeat the process until the number equals 1 (where it will stay), or it 
+loops endlessly in a cycle which does not include 1.
+Those numbers for which this process ends in 1 are happy.
+Return true if n is a happy number, and false if not.'''
+
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        counter = 0
+        while counter < 1000:
+            n = str(n)
+            total = 0
+            for digit in n:
+                total += int(digit)**2
+            if total == 1:
+                return True
+            else:
+                n = total
+            counter += 1
+        return False
